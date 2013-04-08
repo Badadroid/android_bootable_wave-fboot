cb6c4f0dd63820868bb8f1de41dd86d3ebf63416
idonthavenick <ikincidenemehot@hotmail.com>
2013-04-08 18:36:16 +0300

    Testing this for booting

--- src/FOTA.c
+++ src/FOTA.c
@@ -53,32 +53,36 @@ int main(runMode_t mode)
    __PfsNandInit();
    __PfsMassInit();
    MemoryCardMount();
+   disp_FOTA_Printf("Mounted Sd Card");
    tfs4_stat(kernelImage, &filestat);
    kernelSize = filestat.st_size;
    if ((fd=tfs4_open(kernelImage, 4)) >= 0)
    {
       tfs4_read(fd, &KERNEL_BUF, kernelSize);
+      disp_FOTA_Printf("Readed kernel");
       tfs4_close(fd);
    }   
    
 
    DisableMmuCache(mmuctrl);
+   disp_FOTA_Printf("Disabled MMU");
    _CoDisableMmu();
    
    //DRV_Modem_BootingStart
    
    setup_core_tag(ATAG_buf);
+   disp_FOTA_Printf("setup atag");
    setup_serial_tag(0x123, 0x456);
    setup_rev_tag('0');
-   if (mode == rm_FOTA_RECOVERY)
-      setup_cmdline_tag(cmdlnRM);
-   else
+   //if (mode == rm_FOTA_RECOVERY)
+     // setup_cmdline_tag(cmdlnRM);
+   //else
       setup_cmdline_tag(cmdln);
    setup_end_tag();
    
    //copy kernel to the right position
    memcpy(&KERNEL_START, &KERNEL_BUF, kernelSize);
-   
+   disp_FOTA_Printf("Copied kernel to boot");
    //SYSCON operations
    *((unsigned int*)SYSCON_NORMAL_CFG) = 0xFFFFFFFF; 
    _CoDisableDCache();
@@ -88,10 +92,11 @@ int main(runMode_t mode)
    
    kernel = (fun_t)&KERNEL_START;
    kernel(0, 0x891, ATAG_buf);
+   disp_FOTA_Printf("it must start now");
    
    //loop forever
-   while(1);
-   
+   //while(1);
+   //disp_FOTA_Printf("sh*t loop time");
    return 0;
 
 }
